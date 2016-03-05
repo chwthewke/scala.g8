34068f2fab36942a581a152875b3e66b43fd3343
Chewie <chwthewke@gmail.com>
2016-03-05 13:03:22 +0100

    [scala.g8] Lib updates, monocle and partial buildinfo src removal

--- src/main/g8/project/SbtBuildInfo.scala
+++ src/main/g8/project/SbtBuildInfo.scala
@@ -4,19 +4,20 @@ import sbtbuildinfo.Plugin._
 import sbtrelease.Version
 
 object SbtBuildInfo {
-  val shortVersion = SettingKey[String]("short-version")
+  val shortVersion = SettingKey[String]( "short-version" )
 
-  def buildSettings(targetPackage: String = "") = buildInfoSettings ++ Seq(
+  def buildSettings( targetPackage: String = "" ) = buildInfoSettings ++ Seq(
     sourceGenerators in Compile <+= buildInfo,
     buildInfoPackage := targetPackage,
     buildInfoObject := upperCamel( name.value ),
-    shortVersion := shortenVersion(version.value),
-    buildInfoKeys := Seq(version, shortVersion),
-    managedSourceDirectories in Compile += (sourceManaged in Compile).value / "sbt-buildinfo")
+    shortVersion := shortenVersion( version.value ),
+    buildInfoKeys := Seq( version, shortVersion ) )
 
-  private def shortenVersion(version: String): String = Version(version) match {
-    case Some(Version(major, minor, _, _)) => Version(major, minor, None, None).string
-    case _                                 => throw new IllegalArgumentException(s"Could not parse version \$version")
+  private def shortenVersion( version: String ): String = Version( version ) match {
+    case Some( Version( major, minor, _, _ ) ) =>
+      Version( major, minor, None, None ).string
+    case _ => 
+      throw new IllegalArgumentException( s"Could not parse version \$version" )
   }
 
   def startCase( s : String ) = s.toLowerCase.split( " " ).map( _.capitalize ).mkString( " " )
